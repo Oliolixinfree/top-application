97f151468c0927e53875ed661ddf070f535b1bca
Ivan <offlane@tut.by>
2022-09-23 20:11:27 +0300

    Ptag component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, Ptag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -13,6 +13,21 @@ export default function Home(): JSX.Element {
       <Button appearance="ghost" arrow="down">
         Кнопка2
       </Button>
+      <Ptag size="small">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem cum impedit officia
+        et? Sed praesentium nihil illo. Suscipit quo asperiores velit repudiandae vero fugit,
+        laudantium ipsam voluptatum nesciunt inventore cupiditate!
+      </Ptag>
+      <Ptag size="medium">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quae sint at dolorem
+        similique, pariatur, molestiae nesciunt, quia veniam itaque laborum aspernatur libero.
+        Magni, omnis dolore rem totam provident commodi.
+      </Ptag>
+      <Ptag size="large">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit laudantium, laboriosam
+        quod odio expedita nemo quibusdam repellat, id qui architecto esse ad nihil accusamus eius,
+        est iusto omnis cupiditate at.
+      </Ptag>
     </>
   );
 }
