b39572bdcf9f7a79b7027b0b337e969926053128
Ivan <offlane@tut.by>
2022-09-30 21:33:56 +0300

    adding logic and styling to the TopPageComponent.

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,11 +1,33 @@
-import styles from './Ptag.module.css';
-import cn from 'classnames';
+import styles from './TopPageComponent.module.css';
+// import cn from 'classnames';
 import { TopPageComponentProps } from './TopPageComponent.props';
+import { HhData, Htag, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
 
 export const TopPageComponent = ({
   page,
   firstCategory,
   products,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.title}>
+        <Htag tag="h1">{page.title}</Htag>
+        {products && (
+          <Tag color="gray" size="m">
+            {products.length}
+          </Tag>
+        )}
+        <span>Sort</span>
+      </div>
+      <div>{products && products.map((p) => <div key={p._id}>{p.title}</div>)}</div>
+      <div className={styles.hhTitle}>
+        <Htag tag="h2">Вакансии - {page.category}</Htag>
+        <Tag color="red" size="m">
+          hh.ru
+        </Tag>
+      </div>
+      {firstCategory === TopLevelCategory.Courses && <HhData {...page.hh} />}
+    </div>
+  );
 };
