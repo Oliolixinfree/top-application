1cec0d68a5e355b018d939f4819db8cf9d60fb00
Ivan <offlane@tut.by>
2022-10-07 15:24:31 +0300

    Bug fix useReducer.

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,10 +1,9 @@
 import styles from './TopPageComponent.module.css';
-// import cn from 'classnames';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import { Advantages, HhData, Htag, Product, Ptag, Sort, Tag } from '../../components';
 import { TopLevelCategory } from '../../interfaces/page.interface';
 import { SortEnum } from '../../components/Sort/Sort.props';
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { sortReducer } from './sort.reducer';
 
 export const TopPageComponent = ({
@@ -20,6 +19,10 @@ export const TopPageComponent = ({
   const setSort = (sort: SortEnum) => {
     dispatchSort({ type: sort });
   };
+
+  useEffect(() => {
+    dispatchSort({ type: 'reset', initialState: products });
+  }, [products]);
 
   return (
     <div className={styles.wrapper}>
