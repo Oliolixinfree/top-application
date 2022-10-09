af3539f5f436364bf0b353125676873e1ca6715a
Ivan <offlane@tut.by>
2022-10-09 21:59:15 +0300

    Up component created. Implemented scroll up on button click.

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -5,6 +5,7 @@ import { LayoutProps } from './Layout.props';
 import { Sidebar } from './Sidebar/Sidebar';
 import styles from './Layout.module.css';
 import { AppContextProvider, IAppContext } from '../context/app.context';
+import { Up } from '../components';
 
 const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
   return (
@@ -13,6 +14,7 @@ const Layout = ({ children, ...props }: LayoutProps): JSX.Element => {
       <Sidebar className={styles.sidebar} />
       <div className={styles.body}>{children}</div>
       <Footer className={styles.footer} />
+      <Up />
     </div>
   );
 };
